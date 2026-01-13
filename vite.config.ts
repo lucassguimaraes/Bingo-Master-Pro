3a05def78a9140002c71e702b510a069ae1a7cf5
Lucas Guimarães <138685347+lucassguimaraes@users.noreply.github.com>
2026-01-13 15:39:31 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // ADICIONE ESTA LINHA ABAIXO:
+      base: '/Bingo-Master-Pro/', 
       server: {
         port: 3000,
         host: '0.0.0.0',
